ca1b3769122c66c938c1bf54a8dfb1c388ada284
Nabil Hayet <nabil.bracu@gmail.com>
2020-05-10 06:36:33 +0000

    fifth commit

--- Movies/lib/world/scrapper.rb
+++ Movies/lib/world/scrapper.rb
@@ -8,9 +8,36 @@ def self.get_movie
   doc = Nokogiri::HTML(html)
   post= doc.css("div .movie_popup")
     name = []
-    post.search("h6").select do |a|
-    name << a.text
-      end
-      binding.pry
+    cast =[]
+    director =[]
+    producer =[]
+    writer=[]
+    editor=[]
+    cinemato=[]
+    produc_co=[]
+
+    post.search("h6").each do |m_name|
+    name << m_name.text.split("(")[0].strip
+  end
+
+    post.css("p.Cast").each do |b|
+    cast << b.text.chomp("Cast:").strip
+  end
+
+
+    post.css("p.Directors").each do |c|
+    director << c.text.chomp("Cast:").strip
+  end
+  binding.pry
+    post.css("p.Producer").each do |d|
+    producer << d.text.chomp("Cast:").strip
+  end
+
+    post.css("p.Writer").each do |e|
+    writer << e.text.chomp("Cast:").strip
+  end
+
+
+
     end
 end
